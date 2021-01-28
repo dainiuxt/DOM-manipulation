d1461f4516783811e3cb587b6f388555f4c46208
Dainius <dainiuxt@gmail.com>
2021-01-28 09:10:15 +0000

    Add button with event listener via DOM

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const blueH3 = document.createElement('h3');
 const divBlackBorder = document.createElement('div');
 const para2 = document.createElement('p');
 const h1 = document.createElement('h1');
+const helloButton = document.createElement('button');
 
 content.classList.add('content');
 content.textContent = 'This document is created with DOM. This black text is in a DIV.';
@@ -16,10 +17,18 @@ blueH3.textContent = 'I\'m blue H3!';
 divBlackBorder.style.border = "solid black 2px";
 para2.textContent = 'Me too!';
 h1.textContent = 'I\'m in a DIV with a black border.'
+helloButton.classList.add('btn');
+helloButton.textContent = 'Click me!';
 
 container.appendChild(content);
 content.appendChild(para);
 content.appendChild(blueH3);
 content.appendChild(divBlackBorder);
 divBlackBorder.appendChild(h1);
+divBlackBorder.appendChild(helloButton);
 divBlackBorder.appendChild(para2);
+
+helloButton.addEventListener('click', (e) => {
+  alert('Hello, World!');
+  e.target.style.background = 'blue';
+})
