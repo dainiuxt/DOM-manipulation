383423d9bcf9ef318964799d4cbfef424e7b1736
Dainius <dainiuxt@gmail.com>
2021-01-28 08:21:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// your javascript file
+const container = document.querySelector('#container');
+const content = document.createElement('div');
+const para = document.createElement('p');
+const blueH3 = document.createElement('h3');
+const divBlackBorder = document.createElement('div');
+const para2 = document.createElement('p');
+const h1 = document.createElement('h1');
+
+content.classList.add('content');
+content.textContent = 'This document is created with DOM. This black text is in a DIV.';
+para.style.color = 'red';
+para.textContent = 'Hi, I\'m red! I have my own `<p>`.';
+blueH3.style.color = 'blue';
+blueH3.textContent = 'I\'m blue H3!';
+divBlackBorder.style.border = "solid black 2px";
+para2.textContent = 'Me too!';
+h1.textContent = 'I\'m in a DIV with a black border.'
+
+container.appendChild(content);
+content.appendChild(para);
+content.appendChild(blueH3);
+content.appendChild(divBlackBorder);
+divBlackBorder.appendChild(h1);
+divBlackBorder.appendChild(para2);
